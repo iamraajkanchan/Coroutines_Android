c1beb2a6572de5c9dc4dc9c8443c45d451e215cf
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-07 18:52:11 +0530

    Example of job_function

--- app/src/main/java/com/example/coroutines_android/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_android/MainActivity.kt
@@ -16,27 +16,32 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.Main).launch {
-            task1()
-        }
-        CoroutineScope(Dispatchers.Main).launch {
-            task2()
+        CoroutineScope(Dispatchers.IO).launch {
+            printFollowers()
         }
     }
 
-    private suspend fun task1()
+    private suspend fun printFollowers()
     {
         Log.d(TAG , "Coroutines :: task1 : ${Thread.currentThread().name}")
         Log.d(TAG , "Coroutines :: task1 : Starting Task 1")
         delay(1000)
         Log.d(TAG , "Coroutines :: task1 : Ending Task 1")
+        var fbFollowers = 0
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            fbFollowers = getFollowers()
+        }
+        job.join()
+        Log.d(TAG , "MainActivity :: printFollowers :: fbFollowers : $fbFollowers")
     }
 
-    private suspend fun task2()
+    private suspend fun getFollowers() : Int
     {
         Log.d(TAG , "Coroutines :: task2 : ${Thread.currentThread().name}")
         Log.d(TAG , "Coroutines :: task2 : Starting Task 2")
         delay(1000)
         Log.d(TAG , "Coroutines :: task2 : Ending Task 2")
+        delay(5000)
+        return 54
     }
 }
